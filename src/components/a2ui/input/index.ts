5efc116da317726158279000333fd8ed36910f7e
komod0 <21377494+komod0@users.noreply.github.com>
2026-06-04 07:33:36 -0300

    chore(ui): remove legacy A2UI components

--- src/components/a2ui/input/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./Button";
-export * from "./CheckBox";
-export * from "./TextField";
-export * from "./DateTimeInput";
-export * from "./ChoicePicker";
-export * from "./Slider";
--- src/components/a2ui/layout/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./Row";
-export * from "./Column";
-export * from "./List";
-export * from "./Card";
-export * from "./Tabs";
-export * from "./Divider";
-export * from "./Modal";
